aeaff8820ff21ecabf27c53b30300e7dcbfece2f
Sheykin Semyon <semyon.sheikin@humans.net>
2023-09-25 13:22:33 +0500

    shit

--- index.js
+++ index.js
@@ -2,41 +2,39 @@ function showInfo(content) {
     const data = content.split('\n').slice(1).map(str => str.trim());
     console.log(`Count: ${data.length}`);
 
-    const names = getValuesByIndex(data, 0);
-    console.log(`Barbershops: ${[...new Set(names)].sort().join(', ')}`);
+    console.log(`Barbershops: ${[...new Set(getValuesByIndex(data, 0))].sort().join(', ')}`);
 
     const ratings = getValuesByIndex(data, 4).map(n => +n);
     console.log(`Ratings: Min: ${Math.min(...ratings)} Max: ${Math.max(...ratings)}`)
 
-    let yearsOld = 0;
-    let name = '';
+    let yearsOld = 0, name = '', bestSalon = '', cheapestPrice = Number.MAX_VALUE;
 
-    for (let row of data) {
-        let splitRow = row.split(',');
-        let currentYearOld = +splitRow[3];
-        if (currentYearOld > yearsOld) {
-            yearsOld = currentYearOld;
-            name = splitRow[0];
-        }
-    }
+    data.forEach(row => {
+        let [salonName, price, , age] = row.split(',');
+        let [minPrice, maxPrice] = price.split('-').map(Number);
+        let avgPrice = (minPrice + maxPrice) / 2;
+        let currentYearOld = Number(age);
+
+        if (currentYearOld > yearsOld) [yearsOld, name] = [currentYearOld, salonName];
+        if (currentYearOld > 5 && avgPrice < cheapestPrice) [cheapestPrice, bestSalon] = [avgPrice, salonName];
+    })
     console.log(`Oldest barbershop: ${name}`)
-
-    let bestSalon = '';
-    let cheapestPrice = Number.MAX_VALUE;
-
-    for (let row of data) {
-        let splitRow = row.split(',');
-        let yearsOld = +splitRow[3];
-        let priceRange = splitRow[1].split('-');
-        let avgPrice = (+priceRange[0] + +priceRange[1]) / 2;
-        if (yearsOld > 5 && avgPrice < cheapestPrice) {
-            cheapestPrice = avgPrice;
-            bestSalon = splitRow[0];
-        }
-    }
-    console.log(`Best barbershop: ${bestSalon}`);
+    console.log(`Best barbershop: ${bestSalon}`)
 }
 
 function getValuesByIndex(data, index) {
     return data.map(str => str.split(',')[index]);
 }
+
+showInfo(`арикмахерской,Диапазон цен стрижки,Тип парикмахерской,Лет существования,Рейтинг по отзывам
+Стрижка-Мастерская,300-800,Мужская,5,4.7
+Салон Красоты,500-1500,Женская,10,4.9
+Барбершоп Брутал,400-1000,Мужская,3,4.5
+Парикмахерская Престиж,600-2000,Женская,8,4.8
+Мужской Стиль,350-900,Мужская,6,4.6
+Гламур Студия,700-2500,Женская,4,4.7
+Барбершоп Элегант,450-1200,Мужская,2,4.3
+Эксклюзив Салон,1000-3000,Женская,12,4.9
+Хипстерский Барбер,500-1500,Мужская,7,4.5
+Красота и Стиль,400-1000,Женская,9,4.7
+Стрижка Эксперт,300-800,Мужская,4,4.2`)
