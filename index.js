2e8d53fa99cad9d19d388400f3635f8989bb4a7e
Sheykin Semyon <semyon.sheikin@humans.net>
2023-09-25 13:35:49 +0500

    shit

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 function showInfo(content) {
-    const data = content.split('\n').slice(1).map(str => str.trim());
+    const data = content.split('\n').slice(1).map(str => str.trim().split(','));
     console.log(`Count: ${data.length}`);
 
-    console.log(`Barbershops: ${[...new Set(getValuesByIndex(data, 0))].sort().join(', ')}`);
+    console.log(`Barbershops: ${[...new Set(data.map(row => row[0]))].sort().join(', ')}`);
 
-    const ratings = getValuesByIndex(data, 4).map(n => +n);
+    const ratings = data.map(row => Number(row[4]));
     console.log(`Ratings: Min: ${Math.min(...ratings)} Max: ${Math.max(...ratings)}`)
 
     let yearsOld = 0, oldestSalon = '', bestSalon = '', cheapestPrice = Number.MAX_VALUE;
 
     data.forEach(row => {
-        let [salonName, price, , age] = row.split(',');
+        let [salonName, price, , age] = row;
         let [minPrice, maxPrice] = price.split('-').map(Number);
         let avgPrice = (minPrice + maxPrice) / 2;
         let currentYearOld = Number(age);
@@ -20,8 +20,4 @@ function showInfo(content) {
     })
     console.log(`Oldest barbershop: ${oldestSalon}`)
     console.log(`Best barbershop: ${bestSalon}`)
-}
-
-function getValuesByIndex(data, index) {
-    return data.map(str => str.split(',')[index]);
 }
