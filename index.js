8eee0a2725a5a27afdba65029cb7c20d2b290552
Sheykin Semyon <semyon.sheikin@humans.net>
2023-09-25 12:27:00 +0500

    shit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hello, world");
+function showInfo(content) {
+    const data = content.split('\n').slice(1).map(str => str.trim());
+    console.log(`Count: ${data.length}`);
+
+    const names = getValuesByIndex(data, 0);
+    console.log(`Barbershops: ${[...new Set(names)].sort().join(', ')}`);
+
+    const ratings = getValuesByIndex(data, 4).map(n => +n);
+    console.log(`Ratings: Min: ${Math.min(...ratings)} Max: ${Math.max(...ratings)}`)
+}
+
+function getValuesByIndex(data, index) {
+    return data.map(str => str.split(',')[index]);
+}
