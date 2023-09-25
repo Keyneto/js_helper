4deb27587ec3ed6beb2c70197a1b55f48fbd00a5
Sheykin Semyon <semyon.sheikin@humans.net>
2023-09-25 12:37:28 +0500

    shit

--- index.js
+++ index.js
@@ -7,6 +7,19 @@ function showInfo(content) {
 
     const ratings = getValuesByIndex(data, 4).map(n => +n);
     console.log(`Ratings: Min: ${Math.min(...ratings)} Max: ${Math.max(...ratings)}`)
+
+    let yearsOld = 0;
+    let name = '';
+
+    for (let row of data) {
+        let splitRow = row.split(',');
+        let currentYearOld = +splitRow[3];
+        if (currentYearOld > yearsOld) {
+            yearsOld = currentYearOld;
+            name = splitRow[0];
+        }
+    }
+    console.log(`Oldest barbershop: ${name}`)
 }
 
 function getValuesByIndex(data, index) {
