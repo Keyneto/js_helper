c17bb78d1631bcd1683cc94872d6305d76c940e5
Sheykin Semyon <semyon.sheikin@humans.net>
2023-09-25 12:50:17 +0500

    shit

--- index.js
+++ index.js
@@ -20,6 +20,21 @@ function showInfo(content) {
         }
     }
     console.log(`Oldest barbershop: ${name}`)
+
+    let bestSalon = '';
+    let cheapestPrice = Number.MAX_VALUE;
+
+    for (let row of data) {
+        let splitRow = row.split(',');
+        let yearsOld = +splitRow[3];
+        let priceRange = splitRow[1].split('-');
+        let avgPrice = (+priceRange[0] + +priceRange[1]) / 2;
+        if (yearsOld > 5 && avgPrice < cheapestPrice) {
+            cheapestPrice = avgPrice;
+            bestSalon = splitRow[0];
+        }
+    }
+    console.log(`Best barbershop: ${bestSalon}`);
 }
 
 function getValuesByIndex(data, index) {
