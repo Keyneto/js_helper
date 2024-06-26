59007ba9ec4b63e909d98861717f86c20c0a1fa0
Sheykin Semyon <semyon.sheikin@humans.net>
2024-06-26 10:52:24 +0400

    гы

--- index.js
+++ index.js
@@ -1,68 +1,16 @@
-function addRegistrationForm() {
-    const formContainer = document.querySelector('.form-container');
+'use strict';
 
-    formContainer.innerHTML = `
-        <form id="registrationForm">
-            <div class="form-group">
-                <label for="inputName">Name</label>
-                <input type="text" class="form-control" id="inputName" placeholder="Input name" name="name" required>
-            </div>
-            <div class="form-group">
-                <label for="inputPhone">Phone</label>
-                <input type="text" class="form-control" id="inputPhone" placeholder="Input phone" name="phone" required>
-            </div>
-            <input type="submit" value="Submit" class="btn btn-primary">
-        </form>
-    `;
+var express = require('express');  // Импортируем express
 
-    const form = document.getElementById('registrationForm');
-    form.addEventListener('submit', handleFormSubmit);
+var app = express();  // Создаем приложение express
 
-    const nameInput = document.getElementById('inputName');
-    const phoneInput = document.getElementById('inputPhone');
+app.get('/', function(req, res){
+    res.send('Hello World');
+});
 
-    nameInput.addEventListener('input', () => validateInput(nameInput, validateName));
-    phoneInput.addEventListener('input', () => validateInput(phoneInput, validatePhone));
-}
-
-async function handleFormSubmit(event) {
-    event.preventDefault();
-
-    const form = event.target;
-    const formData = new FormData(form);
-    const jsonData = Object.fromEntries(formData.entries());
-
-    const response = await fetch('/people', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json' // Указываем, что отправляем данные в формате JSON
-        },
-        body: JSON.stringify(jsonData)
+/* istanbul ignore next */
+if (!module.parent) {
+    app.listen(3000, () => {
+        console.log('Express started on port 3000');
     });
-
-    if (response.ok) {
-        document.body.innerHTML = '<h3 class="mb-4">User successfully registered</h3>';
-    }
 }
-
-function validateInput(inputElement, validationFunction) {
-    const isValid = validationFunction(inputElement.value);
-    if (isValid) {
-        inputElement.classList.add('is-valid');
-        inputElement.classList.remove('is-invalid');
-    } else {
-        inputElement.classList.add('is-invalid');
-        inputElement.classList.remove('is-valid');
-    }
-}
-
-function validateName(name) {
-    return name.trim().length > 0;
-}
-
-function validatePhone(phone) {
-    const phoneRegex = /^\+[0-9]{10}$/;
-    return phoneRegex.test(phone.replace(/\s+/g, ''));
-}
-
-export default addRegistrationForm;
